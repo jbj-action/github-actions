93e9361e7dce6b71bdc26545a2656dd40ba897be
DESKTOP-UCG2H7Q\정병재 <wjdqudwo0@naver.com>
2024-12-23 17:20:09 +0900

    cicd-1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction after1234
+          Learn GithubAction after12345
         </a>
       </header>
     </div>
